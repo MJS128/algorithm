bc210bd01b8e09a4b3d0889c326d626495320983
MJS128 <79820612+MJS128@users.noreply.github.com>
2021-03-12 22:19:21 +0900

    for문 수정

--- algorithm/Fly.py
+++ algorithm/Fly.py
@@ -1,15 +1,15 @@
-t = int(input())
-
-for _ in range(t):
-    x, y = map(int,input().split())
-    distance = y - x
-    count = 0  # 이동 횟수
-    move = 1  # count별 이동 가능한 거리
-    move_plus = 0  # 이동한 거리의 합
-    while move_plus < distance :
-        count += 1
-        move_plus += move  # count 수에 해당하는 move를 더함
-        if count % 2 == 0 :  # count가 2의 배수일 때, 
-            move += 1  
-    print(count)
-
+import math
+T = int(input())
+for _ in range(T):
+    x, y = map(int, input().split())
+    diff = int(y - x)
+    if diff <= 3:
+        print(diff)
+    else:
+        n = int(math.sqrt(diff))
+        if diff == n ** 2:
+            print(2*n-1)
+        elif n ** 2 < diff <= n ** 2 + n:
+            print(2*n)
+        else:
+            print(2*n+1)
